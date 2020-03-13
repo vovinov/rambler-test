a87bfc84fdf92d33f7dd6e491d6e2dee174c3a58
slavarikus <39729510+slavarikus@users.noreply.github.com>
2020-03-13 16:18:59 +0300

    Updates

---
+++ precache-manifest.a566443fb4de31aa407cbbab694d023d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad1f163ff874748fba74139fa57ca3c",
+    "url": "/rambler-test/index.html"
+  },
+  {
+    "revision": "2aa28336da1c99c0c718",
+    "url": "/rambler-test/static/css/main.4365c05c.chunk.css"
+  },
+  {
+    "revision": "9be50f460fd1bf2527f1",
+    "url": "/rambler-test/static/js/2.8b1377cd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/rambler-test/static/js/2.8b1377cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa28336da1c99c0c718",
+    "url": "/rambler-test/static/js/main.9f5a337f.chunk.js"
+  },
+  {
+    "revision": "6e345b5a2e813d5ff3d8",
+    "url": "/rambler-test/static/js/runtime-main.cc92872f.js"
+  }
+]);
